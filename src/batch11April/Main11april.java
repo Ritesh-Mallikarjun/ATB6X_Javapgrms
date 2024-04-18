2bffe01ed55c9f0b7ae7f1e724557db574452823
Lenovo <riteshsidhu23@gmail.com>
2024-04-18 08:35:43 +0530

    ddd

---
+++ src/batch11April/Main11april.java
@@ -0,0 +1,17 @@
+package batch11April;
+
+import java.util.Arrays;
+
+public class Main11april {
+    public static void main(String[] args) {
+        System.out.println("hi package , how are you");
+        System.out.print("hello thursday");
+        System.out.print("hello sday\n");
+        System.out.print("hello sday1");
+        System.out.print("hello sday2");
+
+        int[] arr={1,4,56,67};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-1]);
+    }
+}
